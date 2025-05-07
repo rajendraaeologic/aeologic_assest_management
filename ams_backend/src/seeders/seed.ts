3e13cddfdfbe5e395fe112595e57e5fcce3f3594
Rajat K <rajendra@aeologic.com>
2025-05-07 07:51:06 +0530

    remove unused files and code

--- ams_backend/src/seeders/seed.ts
+++ ams_backend/src/seeders/seed.ts
@@ -55,54 +55,54 @@ const topics = {
 }
 
 async function seedCategories() {
-    return categories.map((category) =>
-        prisma.category.upsert({
-            where: { slug: category.slug },
-            update: {},
-            create: category,
-        })
-    )
+    // return categories.map((category) =>
+    //     prisma.category.upsert({
+    //         where: { slug: category.slug },
+    //         update: {},
+    //         create: category,
+    //     })
+    // )
 }
 
 async function seedTopics() {
-    const topicOperations: any[] = []
-    for (const [slug, topicData] of Object.entries(topics)) {
-        const category = await prisma.category.findUnique({
-            where: { slug },
-        })
-
-        if (category) {
-            const topicUpserts = topicData.map((topic) =>
-                prisma.topic.upsert({
-                    where: { slug: topic.slug },
-                    update: {},
-                    create: {
-                        ...topic,
-                        categoryId: category.id,
-                    },
-                })
-            )
-            topicOperations.push(...topicUpserts)
-        } else {
-            console.log(`Category with slug "${slug}" not found, skipping topics for this category.`);
-        }
-    }
-
-    return topicOperations
+    // const topicOperations: any[] = []
+    // for (const [slug, topicData] of Object.entries(topics)) {
+    //     const category = await prisma.category.findUnique({
+    //         where: { slug },
+    //     })
+    //
+    //     if (category) {
+    //         const topicUpserts = topicData.map((topic) =>
+    //             prisma.topic.upsert({
+    //                 where: { slug: topic.slug },
+    //                 update: {},
+    //                 create: {
+    //                     ...topic,
+    //                     categoryId: category.id,
+    //                 },
+    //             })
+    //         )
+    //         topicOperations.push(...topicUpserts)
+    //     } else {
+    //         console.log(`Category with slug "${slug}" not found, skipping topics for this category.`);
+    //     }
+    // }
+    //
+    // return topicOperations
 }
 
 
 async function main() {
     const startTime = Date.now()
     try {
-        const categoryOperations = await seedCategories();
-        await prisma.$transaction(categoryOperations);
-
-        const topicOperations = await seedTopics();
-        await prisma.$transaction(topicOperations);
-
-        const endTime = Date.now()
-        console.log(`Seeding completed in ${((endTime - startTime) / 1000).toFixed(2)} seconds`)
+        // const categoryOperations = await seedCategories();
+        // await prisma.$transaction(categoryOperations);
+        //
+        // const topicOperations = await seedTopics();
+        // await prisma.$transaction(topicOperations);
+        //
+        // const endTime = Date.now()
+        // console.log(`Seeding completed in ${((endTime - startTime) / 1000).toFixed(2)} seconds`)
     } catch (error) {
         console.error('Error during seeding: ', error)
     } finally {

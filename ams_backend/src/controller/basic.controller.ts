f7277acd3c62348684d3e6601c5f7ecf3aee092a
ABHINAV ARYAN <aryanabhinav168@gmail.com>
2025-05-16 18:56:09 +0530

    SwaggerUI implementation for BasicRoutes

--- ams_backend/src/controller/basic.controller.ts
+++ ams_backend/src/controller/basic.controller.ts
@@ -1,6 +1,32 @@
 import catchAsync from "@/lib/catchAsync";
 import { basicService } from "@/services";
 
+
+/**
+ * @swagger
+ * /countries:
+ *   get:
+ *     summary: Get all countries
+ *     description: Returns a list of countries
+ *     tags:
+ *       - Basic
+ *     responses:
+ *       200:
+ *         description: List of countries
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 countries:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   example: [ "India", "United States", "Germany" ]
+ *       500:
+ *         description: Internal server error
+ */
+
 const getCountries = catchAsync(async (req, res) => {
   const countries = await basicService.getCountries();
   res.send({ countries });
